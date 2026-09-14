f8bf079f26e7add04233c291eec2d1769052314d
NEERAD <neerad_2024bcse079@nitsri.ac.in>
2026-09-14 20:51:18 +0530

    Initial commit

---
+++ src/main/java/demo/GithubActions/GithubActionsApplication.java
@@ -0,0 +1,13 @@
+package demo.GithubActions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubActionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubActionsApplication.class, args);
+	}
+
+}
---
+++ src/main/java/demo/GithubActions/home.java
@@ -0,0 +1,14 @@
+package demo.GithubActions;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class home {
+    @RequestMapping("/")
+    public String homepage(){
+        return "Hello and Welcome to the test server";
+    }
+}
